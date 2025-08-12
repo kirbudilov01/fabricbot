4f0fe8647b5455777bcd95ac21a711cab642323d
kirbudilov01 <budilov02ke@gmail.com>
2025-08-12 15:07:03 +0700

    Updated my-page.tsx

--- app/my-page.tsx
+++ app/my-page.tsx
@@ -47,8 +47,6 @@ export default function MyPageScreen() {
         : [...prev, subcategoryId]
     );
   };
-    );
-  };
 
   const handlePay = (product: any) => {
     setSelectedProduct(product);
